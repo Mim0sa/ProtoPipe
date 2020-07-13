1c97267f6804db88730ed4a30464456e87235cb9
mimosa <563576965@qq.com>
2020-07-13 13:55:01 +0800

    add folders in model & scene

---
+++ ProtoPipe/ProtoPipe/Scene/CanvasViewController/Inspector/PPInspectorPageView.swift
@@ -0,0 +1,21 @@
+//
+//  PPInspectorPageView.swift
+//  ProtoPipe
+//
+//  Created by CM on 2020/7/13.
+//  Copyright © 2020 吉乞悠. All rights reserved.
+//
+
+import UIKit
+
+class PPInspectorPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ProtoPipe/ProtoPipe/Scene/CanvasViewController/Navigator/PPNavigatorPageView.swift
@@ -0,0 +1,21 @@
+//
+//  PPNavigatorPageView.swift
+//  ProtoPipe
+//
+//  Created by CM on 2020/7/13.
+//  Copyright © 2020 吉乞悠. All rights reserved.
+//
+
+import UIKit
+
+class PPNavigatorPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
